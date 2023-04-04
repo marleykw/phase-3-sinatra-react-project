9d4b2faad10c8f30df7c5dda4798db13116c2e3b
Marley Watson <marleykw17@gmail.com>
2023-04-04 14:34:21 -0400

    updating models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,18 @@ class ApplicationController < Sinatra::Base
     outfits.to_json
   end
 
+  delete "/closet/:id" do
+    outfit = Outfit.find(params[:id])
+
+    outfit.destroy
+
+    outfit.to_json
+  end
+
+  post "/closet" do
+    outfit = Outfit.create(
+      
+    )
+  end
+
 end
