0bdd45fa2b42db854d033ee9b7622d1a2bd2ce1b
Marley Watson <marleykw17@gmail.com>
2023-05-16 08:32:34 -0400

    Updating by getting rid of extra stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  before do 
+    response.headers['Access-Control-Allow-Origin'] = "*"
   end
 
   get "/closet" do
     outfits = Outfit.all
-    outfits.to_json
+    outfits.to_json(:include=>[
+      :articles,
+      :comments
+    ])
   end
 
-  delete "/closet/:id" do
-    outfit = Outfit.find(params[:id])
-
-    outfit.destroy
-
-    outfit.to_json
+  get "/comments/:id" do 
+    comments = Comment.find(params[:id])
+    comments.to_json
   end
 
-  post "/closet/:id" do
-    outfit = Outfit.create(
+  delete "/comments/:id" do 
+    comment = Comment.find(params[:id])
+    comment.destroy
+    comment.to_json
+  end
 
-    )
+
+  post "/closet" do
+    comment = Comment.create(comment:params[:comment], outfit_id:params[:outfit_id])
+    comment.to_json
   end
 
 end
