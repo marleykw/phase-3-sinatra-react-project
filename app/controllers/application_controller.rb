b66beef2e04c47e4c000131a3d0ccf0735d2d95b
Marley Watson <marleykw17@gmail.com>
2023-06-05 18:19:39 -0400

    Updating to properly pull comments during the index instead of on the detail page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,28 +5,23 @@ class ApplicationController < Sinatra::Base
     response.headers['Access-Control-Allow-Origin'] = "*"
   end
 
-  get "/closet" do
-    items = Article.all
-    items.to_json
+  get "/articles" do
+    items = Article.all.includes(:comments)
+    items.to_json(:include => :comments)
   end
 
-  post "/closet" do
+  post "/articles" do
     item = Article.create(name:params[:name], url:params[:image], price:params[:price], purchase_date:params[:date])
     item.to_json
   end
 
-  patch "/closet/:id" do
+  patch "/articles/:id" do
     item = Article.find(params[:id])
     item.update(last_worn: params[:last_worn])
     item.to_json
   end
 
-  get "/closet/:id" do
-    item = Article.includes(:comments).find(params[:id])
-    item.to_json(:include => :comments)
-  end
-
-  delete "/closet/:id" do 
+  delete "/articles/:id" do 
     item = Article.find(params[:id])
     item.destroy
     item.to_json
@@ -42,7 +37,6 @@ class ApplicationController < Sinatra::Base
     comment.destroy
     comment.to_json
   end
-
 
   post "/comments" do
     comment = Comment.create(comment:params[:comment], article_id:params[:article_id])
