adde87138cdc1d551727486735a5878d14085ff1
Marley Watson <marleykw17@gmail.com>
2023-06-10 09:33:26 -0400

    updating to have the patch take the price as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/articles/:id" do
     item = Article.find(params[:id])
-    item.update(last_worn: params[:last_worn])
+    item.update(last_worn: params[:last_worn], price: params[:price])
     item.to_json
   end
 
